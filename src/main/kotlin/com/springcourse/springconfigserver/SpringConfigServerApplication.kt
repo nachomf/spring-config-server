e24f67d90b0eedb1fbf799675e374b053912f273
nachoemf <ignacio.fornaso@mercadolibre.com>
2022-02-22 19:48:11 -0300

    continue with more lessons

--- src/main/kotlin/com/springcourse/springconfigserver/SpringConfigServerApplication.kt
+++ src/main/kotlin/com/springcourse/springconfigserver/SpringConfigServerApplication.kt
@@ -2,7 +2,9 @@ package com.springcourse.springconfigserver
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.config.server.EnableConfigServer
 
+@EnableConfigServer
 @SpringBootApplication
 class SpringConfigServerApplication
 
